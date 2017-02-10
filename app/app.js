1dca9514e987b2d61a69ad3c96ef43ab37021208
Omar Almootassem <218omar.a@gmail.com>
2017-02-09 22:31:54 -0500

    added file upload dialog with drag-n-drop using lf-ng-md-file-input

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', ['ngMaterial', 'ngAvatar',
+angular.module('myApp', ['ngMaterial', 'ngAvatar', 'lfNgMdFileInput',
   'ngRoute',
   'myApp.view1',
   'myApp.view2',
@@ -11,4 +11,24 @@ config(['$locationProvider', '$routeProvider', function($locationProvider, $rout
   $locationProvider.hashPrefix('!');
 
   $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+}])
+
+.controller('NavCtrl', function($scope, $mdDialog) {
+	$scope.name = "Omar Almootassem";
+
+	$scope.showAddDialog = function(ev){
+		$mdDialog.show({
+			controller: AddDialogController,
+			templateUrl: 'dialogs/add-item.tmpl.html',
+			parent: angular.element(document.body),
+			targetEvent: ev,
+			clickOutsideToClose: true,
+		});
+	};
+
+	function AddDialogController($scope, $mdDialog){
+	    $scope.cancel = function() {
+	      $mdDialog.cancel();
+	    };
+	}
+});
